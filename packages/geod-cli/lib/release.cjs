e0db8af269ae090b7cc9a9a72dfb05953f50daf9
gaopengbin <1804287165@qq.com>
2026-09-24 12:43:55 +0800

    Freeze GeoD CLI 0.3.0 release hashes

--- packages/geod-cli/lib/release.cjs
+++ packages/geod-cli/lib/release.cjs
@@ -2,9 +2,9 @@
 
 // Frozen upstream portable release. Update these together for a new package version.
 module.exports = Object.freeze({
-  version: "0.2.0",
+  version: "0.3.0",
   platform: "windows-x64",
-  sourceRevision: "b8aba2d15b7b7ccccbe98f3fe05f3dcd74f79862",
-  portableZipSha256: "b1dc4fd0d53bd284d35a981d4a611ed0f3f59b08c137823464e549b563f89cae",
-  binarySha256: "70f7c507606cef384de177695e6e0ae3aac7cbd4bacae27ac1ea63a929bd3706",
+  sourceRevision: "144c09fe3b353056a4bcd03ceb2c15944201cee0",
+  portableZipSha256: "e3ff20021b4e826aac96ae7b4f210f9612ffa18f2d2ae5ea95a281f84ea7fda5",
+  binarySha256: "85bc52c8c5c6580f90adf96b6760050f51cc48695ed7e03fecc8e484d5e51906",
 });
